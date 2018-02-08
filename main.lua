efb19186a1eb8e68c26ccef1834852d40001c383
fawazmez <fawaz.mezher@ocsbstudent.ca>
2018-02-08 14:30:11 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-- Created by: Fawaz Mezher
+-- Created on: Feb 2018
+--
+-- color background
+-----------------------------------------------------------------------------------------
+display.setDefault( "background", 36, 28, 1 )
+local image = display.newImageRect( "image.png", 1000 , 1000)
+image.X = 4000
+image.y = 1000
